97dd096d1c8042bd9c49acbe508abdc8bee07978
Seo <sdm067@naver.com>
2023-02-17 16:20:03 +0900

    LectureStudent Mapper 검색 조건 수정함

--- ServerProject/src/main/java/com/acorn2/FinalProject/lectureStudent/dto/req/LectureStudentReadReqDto.java
+++ ServerProject/src/main/java/com/acorn2/FinalProject/lectureStudent/dto/req/LectureStudentReadReqDto.java
@@ -8,18 +8,28 @@ public class LectureStudentReadReqDto extends PagingDto{
 	private int lec_Stu_Ref_Group;
 	private String large_category;
 	private String small_category;
+	private String id;
+	private String birth;
+	private String phone;
+	private String email;
+	private String User_Regdate;
 
 	
 	public LectureStudentReadReqDto() {}
 
 
 	public LectureStudentReadReqDto(String lec_Stu_User_Id, int lec_Stu_Ref_Group, String large_category,
-			String small_category) {
+			String small_category, String id, String birth, String phone, String email, String user_Regdate) {
 		super();
 		Lec_Stu_User_Id = lec_Stu_User_Id;
 		this.lec_Stu_Ref_Group = lec_Stu_Ref_Group;
 		this.large_category = large_category;
 		this.small_category = small_category;
+		this.id = id;
+		this.birth = birth;
+		this.phone = phone;
+		this.email = email;
+		User_Regdate = user_Regdate;
 	}
 
 
@@ -63,10 +73,56 @@ public class LectureStudentReadReqDto extends PagingDto{
 	}
 
 
-	
-	
+	public String getId() {
+		return id;
+	}
 
-	
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getBirth() {
+		return birth;
+	}
+
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getUser_Regdate() {
+		return User_Regdate;
+	}
+
+
+	public void setUser_Regdate(String user_Regdate) {
+		User_Regdate = user_Regdate;
+	}
+
+
 	
 	
 }
