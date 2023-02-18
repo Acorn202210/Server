b5a8b2f80040681f4aa27e5f74ab5569b4c0a1ed
OkHui <100889562+1000oki@users.noreply.github.com>
2023-02-18 15:05:10 +0900

    exception 수정

--- ServerProject/src/main/java/com/acorn2/FinalProject/exception/CustomizedResponseEntityExceptionHandler.java
+++ ServerProject/src/main/java/com/acorn2/FinalProject/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,6 +2,7 @@ package com.acorn2.FinalProject.exception;
 
 import java.util.Date;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -24,7 +25,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	 @ExceptionHandler(Exception.class)
 	 public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
 	     ExceptionResponse exceptionResponse =
-	             new ExceptionResponse("failed", new Date(), ex.getMessage(), request.getDescription(false));
+	             new ExceptionResponse("failed", new Date(), ExceptionUtils.getStackTrace(ex), request.getDescription(false));
 	     return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	 }
 	 
@@ -32,7 +33,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	 @ExceptionHandler(NoticeNotFoundException.class)
 	 public final ResponseEntity<Object> handleNoticeNotFoundException(Exception ex, WebRequest request){
 	     ExceptionResponse exceptionResponse =
-	             new ExceptionResponse("failed", new Date(), ex.getMessage(), request.getDescription(false));
+	             new ExceptionResponse("failed", new Date(), ExceptionUtils.getStackTrace(ex), request.getDescription(false));
 	     return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
 	 }
  
