7c51438dee10a150bb3e9acd585f80f402df097f
OkHui <100889562+1000oki@users.noreply.github.com>
2023-03-17 16:39:57 +0900

    나의 수강 리스트 강의 번호 추가

--- ServerProject/src/main/java/com/acorn2/plec/api/users/dto/res/MyLectureReadResDto.java
+++ ServerProject/src/main/java/com/acorn2/plec/api/users/dto/res/MyLectureReadResDto.java
@@ -2,14 +2,16 @@ package com.acorn2.plec.api.users.dto.res;
 
 public class MyLectureReadResDto {
 	private Integer imageNum;
-	private String CompleteYn;
+	private String completeYn;
+	private Integer lecNum;
 	
 	public MyLectureReadResDto() {}
 		
-	public MyLectureReadResDto(Integer imageNum, String completeYn) {
+	public MyLectureReadResDto(Integer imageNum, String completeYn, Integer lecNum) {
 		super();
 		this.imageNum = imageNum;
-		CompleteYn = completeYn;
+		this.completeYn = completeYn;
+		this.lecNum = lecNum;
 	}
 
 	public Integer getImageNum() {
@@ -18,12 +20,23 @@ public class MyLectureReadResDto {
 	public void setImageNum(Integer imageNum) {
 		this.imageNum = imageNum;
 	}
+
 	public String getCompleteYn() {
-		return CompleteYn;
+		return completeYn;
 	}
-	public void setComplete_Yn(String completeYn) {
-		CompleteYn = completeYn;
+
+	public void setCompleteYn(String completeYn) {
+		this.completeYn = completeYn;
 	}
+
+	public Integer getLecNum() {
+		return lecNum;
+	}
+
+	public void setLecNum(Integer lecNum) {
+		this.lecNum = lecNum;
+	}
+	
 	
 	
 }
